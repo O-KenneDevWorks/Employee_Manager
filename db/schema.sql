d1c76f88862541e26b127610ee85249ea94e8a4a
OKenne <79246841+LuciferGreyInc@users.noreply.github.com>
2024-10-08 19:21:29 -0400

    Removed excess spacing

--- db/schema.sql
+++ db/schema.sql
@@ -40,10 +40,3 @@ CREATE TABLE employees (
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
     FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
-
-
-
-
-
-
-
